08620efc40809cddb2453fe796c1fec329288a6b
Yeva Sokhatska <evasahat@icloud.com>
2023-09-03 18:26:56 +0200

    user interface

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,30 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app.fxml"));
+        Scene scene = new Scene((Parent) fxmlLoader.load(), 400, 100);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main( String[] args ){
+       // System.out.println( "Hello World!" );
+        launch();
     }
 }
